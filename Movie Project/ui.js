0c36215b0f1875ae062b4ad2b390121c54ea2a33
Tunahan Özcan <ilyastunahanozcan@gmail.com>
2021-06-07 15:40:17 +0300

    movie and director filter added

--- Movie Project/ui.js
+++ Movie Project/ui.js
@@ -58,4 +58,17 @@ class UI{
             filmList.firstElementChild.remove();
         }
     }
+    
+        static tabloarama() {
+               var e, t, n, a, l;
+               for (e = document.getElementById("aramakutusu").value.toUpperCase(), t = document.getElementById("tfilms").getElementsByTagName("tr"), l = 1; l < t.length; l++) {
+                 t[l].style.display = "none", n = t[l].getElementsByTagName("td");
+                 for (var m = 1; m < n.length; m++)
+                   if ((a = t[l].getElementsByTagName("td")[m]) && a.innerHTML.toUpperCase().indexOf(e) > -1) {
+                     t[l].style.display = "";
+                     break
+                   }
+               }
+             }
+           
 }
